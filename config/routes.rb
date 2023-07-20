6e2d332f27aaab7db0f7dcd72a7aec6b0e047d34
haruto <138562348+haru-shducks@users.noreply.github.com>
2023-07-20 06:07:16 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books
+  root 'books#top'
+  #patch '/books/:id', to: 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
